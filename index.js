4aa773500044cb3d21bfc7cbb861930b45d4e11f
KevinChips <kevsilva07@gmail.com>
2019-07-05 16:44:29 -0300

    laptop detail page

--- index.js
+++ index.js
@@ -14,7 +14,24 @@ const server = http.createServer((req, res) => {
     res.end("Products page");
   } else if (pathName === "/laptop" && id < laptopData.length) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`Laptop detail page for laptop id: ${id}`);
+    fs.readFile(
+      `${__dirname}/templates/template-laptop.html`,
+      "utf-8",
+      (error, data) => {
+        let output = data.replace(
+          /{%PRODUCTNAME%}/g,
+          laptopData[id].productName
+        );
+        output = output.replace(/{%PRICE%}/g, laptopData[id].price);
+        output = output.replace(/{%SCREEN%}/g, laptopData[id].screen);
+        output = output.replace(/{%CPU%}/g, laptopData[id].cpu);
+        output = output.replace(/{%STORAGE%}/g, laptopData[id].storage);
+        output = output.replace(/{%RAM%}/g, laptopData[id].ram);
+        output = output.replace(/{%DESCRIPTION%}/g, laptopData[id].description);
+        output = output.replace(/{%IMG%}/g, laptopData[id].image);
+        res.end(output);
+      }
+    );
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("URL was not found on the server");
