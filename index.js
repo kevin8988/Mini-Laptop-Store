2492f2f022524819c23dab565307b37e5c04bb05
KevinChips <kevsilva07@gmail.com>
2019-07-05 16:20:24 -0300

    routing

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const fs = require("fs");
 const http = require("http");
-const url = require("url")
+const url = require("url");
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
+  const pathName = url.parse(req.url, true).pathname;
+  const id = url.parse(req.url, true).query.id;
 
-  const query = url.parse(req.url, true);
-  console.log(query);
-  
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("This is the response");
+  if (pathName === "/products" || pathName === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Products page");
+  } else if (pathName === "/laptop" && id < laptopData.length) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`Laptop detail page for laptop id: ${id}`);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("URL was not found on the server");
+  }
 });
 server.listen(1337, "127.0.0.1", () => {
   console.log("Listen for requests now");
